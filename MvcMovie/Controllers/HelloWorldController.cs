47eeb9dd52b2f8341c50a31c0783feb3ea116ad0
TakashiOka <oka0426@gmail.com>
2017-03-16 15:44:21 +0900

    Add

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication3.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //
+        // GET: /HelloWorld/
+        public ActionResult Index()
+        {
+            //return "This is my <b>default</b> action...";
+            return View();
+        }
+
+        public string Welcom(string name, int numTimes = 1)
+        {
+            return HttpUtility.HtmlEncode("Hello " + name + ", NumTImes is: " + numTimes);
+        }
+    }
+}
